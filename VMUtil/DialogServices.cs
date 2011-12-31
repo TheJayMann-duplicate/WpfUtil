57851eea187f9f01e4b98655b973278eeace27fd
TheJayMann <themann@indyfantasysports.net>
2011-12-31 11:11:01 -0500

    Fixed incorrect namespace

--- VMUtil/DialogServices.cs
+++ VMUtil/DialogServices.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 
-namespace WpfUtil.Utilities {
+namespace VMUtil {
 
     /// <summary>
     /// The DialogServices class is used to allow code to display various dialogs without
